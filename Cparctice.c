8c189aac28e3ee4f0391ae2e999141012e1fdd80
ParkMinUk <parkmxxuk@gmail.com>
2023-06-22 13:50:53 +0900

    C언어 환경 세팅

---
+++ Cparctice.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+    int a,b;
+    scanf("%d %d",&a,&b);
+    printf("저는 정보통신공학부 %d학년\n%d 학번 박민욱입니다\n",a,b);
+
+    system("pause");
+    return 0;
+}
