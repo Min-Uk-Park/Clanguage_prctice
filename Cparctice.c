58183bce2d8bc2cf8324ae36f5f224e82cd6bdb9
ParkMinUk <parkmxxuk@gmail.com>
2023-06-23 19:43:36 +0900

    함수 호출(int형)(2)

--- Cparctice.c
+++ Cparctice.c
@@ -1,70 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <conio.h>
+//#include <conio.h>
 
-// char Grade(int );
-
-// int main(){
-
-//     int score,grade;
-
-//     printf("성적을 입력하시오. : ");
-//     scanf("%d",&score);
-
-//     grade = Grade(score);
-
-//     printf("등급 : %c\n",grade);
-    
-//     system("pause");
-
-// }
-
-// char Grade(int S){
-
-//     char G;
-//     switch(S/10){
-//         case 10 : G='A'; break;
-//         case 9 : G='A'; break;
-//         case 8 : G='B'; break;
-//         case 7 : G='C'; break;
-//         case 6 : G='D'; break;
-//         default : G='F'; break;
-
-//     }
-
-//     return G;
-// }
-
-int caculate(int ,int ,char );
+int Min(int ,int );
 
 int main(){
 
-    int a,b;
-    char x;
+    int a,b,min;
+    
+    
+    
+    while(1){
 
-    printf("2개의 정수 입력 : ");
-    scanf("%2d%2d",&a,&b);
-
-    printf("연산자 입력 [더하기(+), 빼기(-), 곱하기(*), 나누기(/)] : ");
-    scanf("%c%c",&x,&x);
-
-    printf("%3d%3c%3d = %d\n",a,x,b,caculate(a,b,x));
-
-    system("pause");
-
-}
-
-int caculate(int a,int b,char x){
-    int result;
-
-    switch(x){
-
-        case '*' : result = a * b; break;
-        case '+' : result = a + b; break;
-        case '/' : result = a / b; break;
-        case '-' : result = a - b; break;
-
+        scanf("%d%d",&a,&b);
+        
+        if(b==0) break;
+        printf("The lesser of %d and %d is %d\n",a,b,Min(a,b));
+        
     }
 
-    return result;
+    system("pause");
+} 
+    
+int Min(int x,int y){
+    int min;
+
+    min = x > y ? y:x;
+
+    return min;
 }
