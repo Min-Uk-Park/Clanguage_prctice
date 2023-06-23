60d0e75ec70cc8112ab3d2122f28f8cc4d951261
ParkMinUk <parkmxxuk@gmail.com>
2023-06-23 17:39:35 +0900

    반복문(while)

--- Cparctice.c
+++ Cparctice.c
@@ -1,39 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <conio.h>
 
 int main(){
 
-    printf("관계연산자 & 논리연산자 배우기\n형식지정자 기능 배우기\n제어문자 기능 배우기\n");
-    printf("제어문자 : \n /b : backspace, /r : 그 줄의 첫 번째로 이동\n");
-    
+    int i=1,j,k;
 
-    int a=88,max,n,num,maxnum;
-    printf("학생 수: ");
-    scanf("%d",&num);
-    int grade[num];
-    printf("나의 점수 입력 :  ");
-    scanf("%d",&grade[0]);
-    
-    for(n=1;n<num;n++){
-        printf("학생%d의 점수 : ",n);
-        scanf("%d",&grade[n]);
+    while(i<=15){
+        j=1,k=1;
+        while(j<=31-2*i){
+                printf("*");
+                j++;
+        }
+        putchar('\n');
+        while(k<=i){
+            putchar(' ');
+            k++;
+        }
+        i++;
     }
 
-    max = grade[0];
-    maxnum = 0;
-
-    for(n=1;n<num;n++){
-        
-        max = max> grade[n] ? max : grade[n];
-        maxnum = max>grade[n] ? maxnum : n;
-
-        
-    }
-
-    printf("최고 점수는 %d입니다.\n1등 학생은 학생%d입니다.\n ",max,maxnum);
-    
-    
-
-    system("pause");
-    return 0;
+     system("pause");
+     return 0;
 }
--- README.md
+++ README.md
@@ -1,4 +1,3 @@
 # Clanguage_prctice
 Clanguage_practice
-
-## 1일차 : 변수 선언 ~ 반복문 연습 
+    1일차 : 변수 선언 ~ 반복문 연습 
