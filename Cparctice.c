9a40fdf79fc0638d034d88ba1d5639909f007a1e
ParkMinUk <parkmxxuk@gmail.com>
2023-06-24 20:25:10 +0900

    argc, argv 이해

--- Cparctice.c
+++ Cparctice.c
@@ -2,63 +2,13 @@
 #include <stdlib.h>
 //#include <conio.h>
 
-// int fact(int );
+int main(int argc,char *argv[]){
+    int a,b;
 
-// int main(){
-//     int n,i,result;
+    printf("%d\n",argc);
 
-//     printf("정수를 입력하세요. ");
-//     scanf("%d",&n);
-
-//     i=0;
-
-//     while(i<=n){
-//         result = fact(i);
-//         printf("%2d! = %10d\n",i,result);
-//         i++;
-//     }
-
-//     system("pause");   
-// }
-
-// int fact(int n){
-    
-//     int result=1;
-
-//     if(n>1){
-//         result = n * fact(n-1);
-//     }
-
-//     return result; 
-// }
-
-int fact(int n){
-    
-    if(n>=0){
-        if(n<=1){
-            return 1;
-        }
-        else{
-            return (n * fact(n-1));
-        }    
-    }
-  
-}
-
-
-int main(){
-   
-    int n,i,result;
-
-    printf("정수를 입력하세요. ");
-    scanf("%d",&n);
-
-    i=0;
-
-    while(i<=n){
-        result = fact(i);
-        printf("%2d! = %10d\n",i,result);
-        i++;
+    for(a=0;a<=argc;a++){
+        printf("argv[%d] = %s\n",a,argv[a]);
     }
 
     system("pause");
