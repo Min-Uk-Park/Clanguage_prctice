c0bfada18774fe85e8e31130fe775322e840c6ee
ParkMinUk <parkmxxuk@gmail.com>
2023-06-25 16:51:40 +0900

    배열(숫자배열)

--- Cparctice.c
+++ Cparctice.c
@@ -2,16 +2,76 @@
 #include <stdlib.h>
 //#include <conio.h>
 
-//명령프롬프트에서 디버깅 하는 방법으로 해야함 Cparctice.exe입력 후 Enter 하면 실행됨
+// 합계 함수
+int SUM(int score[],int n){
+    int i=0,sum=0;
 
-int main(int argc,char *argv[]){
-    int a,b;
-
-    printf("%d\n",argc);
-
-    for(a=0;a<=argc;a++){
-        printf("argv[%d] = %s\n",a,argv[a]);
+    while(i<n){
+        sum+=score[i];
+        i++;
     }
+    
+    return sum;
+}
+
+//평균 함수
+int avg(int sum,int n){
+    float avg = (float)sum / n;
+
+    return avg;
+}
+
+// 최대값 함수
+int max(int score[],int n){
+    int i=0,max=score[0];
+
+    for(i=1;i<n;i++){
+
+        if(max!=score[i]){
+            if(max<score[i]) {
+                 max = score[i];
+        }
+    }
+      
+}
+    return max;
+}
+
+        
+// 최소값 함수
+int min(int score[],int n){
+    int i,min=score[0];
+
+    for(i=1;i<n;i++){
+        min = min > score[i] ? score[i] : min;
+    }
+
+    return min;
+
+}
+
+// main함수
+int main(int argc, char *argv[]){
+
+    int i=0,n,sum;
+
+    printf("학생 수를 입력하세요. ");
+    scanf("%d",&n);
+    putchar('\n');
+
+    int score[n];
+
+    while(i<n){
+        printf("학생[%d] : ",i+1);
+        scanf("%d",&score[i]);
+        i++;
+    }
+
+    sum = SUM(score,n);
+    printf("\n합계 : %d",sum);
+    printf("\n평균 : %d",avg(sum,n));
+    printf("\n최대값 : %d",max(score,n));
+    printf("\n최소값 : %d\n",min(score,n));
 
     system("pause");
 }
