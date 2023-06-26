7773fb5cc73085883d86e721f9d6b0070f236dff
ParkMinUk <parkmxxuk@gmail.com>
2023-06-27 02:08:15 +0900

    배열 예제(글자 거꾸로 출력)

--- Cparctice.c
+++ Cparctice.c
@@ -1,44 +1,76 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#define _CRT_SECURE_NO_WARNINGS
 //#include <conio.h>
 
-int near(int num[]){
+// void reverse(char major[]){
+//     int i=0;
 
-    int i=0,min,sub,minsub=100;
+    
+//     while(i<strlen(major)-1){
+//         putchar(major[i]);
+//         i++;
+//     }
 
-    while(i<4){
-        if(num[i]!=5){
-            sub = num[i]>5 ? (num[i]-5) : (5-num[i]);
-        }
-        else{
-            sub = 0;
-        }
+//     putchar('\n');
+//     system("pause");
+// }
+   
+// int main(){
 
-        if(sub<minsub){
-            minsub=sub;
-            min=num[i];
-        }
-       i++;
-    }
+//     char major[30];
 
-    return min;
-}
+//     printf("학과를 입력하세요. ");
+//     scanf("%s",major);
+
+//     reverse(major);
+// }
+
 
 int main(){
-    int i,num[5],result;
 
-    for(i=0;i<=4;i++){
-            scanf("%d",&num[i]);
-            
+    char name[12]="abcdef\n",r_name[12]="";
+    int i=0, n, temp;
+   //n=strlen(name);
+
+    printf("%s\n",name);
+    for(i=0;i<=11;i++){
+        if(name[i]=='\n'){
+            break;
         }
-        
-    result = near(num);
+    }
 
-    printf("가장 가까운 값은 : %d\n",result);
+    n=i;
+   
+    printf("%d\n",n);
+
+
+    i=0;
+    while(i<=n-1){
+        
+        r_name[i] = name[n-1-i];
+        i++;
+    }
+    r_name[i]='\n';
+
+    i=0;
+
+    while(i<=n-1){
+        if(r_name[i]<0){
+            temp = r_name[i];
+            r_name[i]=r_name[i+2];
+            r_name[i+2]=temp;
+        }
+
+        i+=3;
+    }
+
+    printf("%s\n",r_name);
+    //puts(r_name);
+    //putchar('\n');
+    
 
     system("pause");
 
-    }
-
-
+   }
