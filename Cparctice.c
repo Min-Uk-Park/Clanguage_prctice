9f0b177305fa373099342a4dd2a525389108184e
ParkMinUk <parkmxxuk@gmail.com>
2023-06-28 20:29:44 +0900

    알고리즘(달팽이)

--- Cparctice.c
+++ Cparctice.c
@@ -4,115 +4,44 @@
 //#define _CRT_SECURE_NO_WARNINGS
 //#include <conio.h>
 
-// int main(){
-//     int a[5][5]={0,},k=1,i=0,j,r=0;
-
-//     while(i<5){
-//         if(i<=2){
-//             j=r;
-//             while(j<5-r){
-//                 a[i][j]=k;
-//                 k++;
-//                 j++;
-//             }
-           
-//         }
-//         else{
-//             j=4-r;
-//             while(j<5-(4-r)){
-//                 a[i][j]=k;
-//                 k++;
-//                 j++;
-//             }
-//         }
-//         i++;
-//         r++;
-        
-//     }
-
-//     i=0;
-//     while(i<5){
-//         j=0;
-//         while(j<5){
-//             if(a[i][j]!=0){
-//             printf("%3d",a[i][j]);
-            
-//             }
-//             else{
-//                 printf("   ");
-//             }
-//             j++;
-//         }
-//         putchar('\n');
-        
-//         i++;
-//     }
-
-//     system("pause");
-// }
-
-
 int main(){
     int a[5][5] = {0,};
-    int i=0,j,k=1,n,r=1,kk=0;
+    int i=0,j,k=1,repeat = 5;
+    int x=0,y=-1,s=1;
 
-    while(i<5){
-        j=0;
-        if(i>0&&i<4){
-                for(n=1;n<=r;n++){
-                    a[i][j]=0;
-                    //r++;
-                    j++;
-                }
-                if(i==2) r=1;
-                else r++;
-            }
-        while(j<5){
-            if(i>0&&i<4){
-                if(i==3)kk=2;
-                if(j>4-i+kk){
-                    
-                    for(n=1;n<=i-kk;n++){
-                        a[i][j] = 0;
-                        j++;
-                    }
-                    
-                }
-                else{
-                    
-                    a[i][j] = k;
-                    k++;
-                    j++;
-            }
-            }
-            else{
-                a[i][j] = k;
-                k++;
-                j++;         
-           
-            }
+    for(;repeat!=0;){
+        i=0;
+        while(i<repeat){
+            y+=s;
+            a[x][y] = k;
+            k++;
+            
+            i++;
         }
-        i++;
+        
+        repeat--;
+        i=0;
+        
+        while(i<repeat){
+            x+=s;
+            a[x][y] = k;
+            k++;
+            i++;
+        }
+        s=-s;
     }
+
     i=0;
     while(i<5){
         j=0;
         while(j<5){
-            if(a[i][j]!=0){
-            printf("%4d",a[i][j]);
-            
-            }
-            else{
-                printf("    ");
-            }
+            printf("%3d",a[i][j]);
             j++;
         }
         putchar('\n');
-        
         i++;
     }
 
     system("pause");
-
 
 }
