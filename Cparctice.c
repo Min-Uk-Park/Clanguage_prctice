6e37229fbc2cc71807b843617ba71cbe8aa3dad5
ParkMinUk <parkmxxuk@gmail.com>
2023-06-28 19:20:30 +0900

    알고리즘(모래시계)

--- Cparctice.c
+++ Cparctice.c
@@ -4,45 +4,115 @@
 //#define _CRT_SECURE_NO_WARNINGS
 //#include <conio.h>
 
-// 배열에 숫자 채우기
+// int main(){
+//     int a[5][5]={0,},k=1,i=0,j,r=0;
+
+//     while(i<5){
+//         if(i<=2){
+//             j=r;
+//             while(j<5-r){
+//                 a[i][j]=k;
+//                 k++;
+//                 j++;
+//             }
+           
+//         }
+//         else{
+//             j=4-r;
+//             while(j<5-(4-r)){
+//                 a[i][j]=k;
+//                 k++;
+//                 j++;
+//             }
+//         }
+//         i++;
+//         r++;
+        
+//     }
+
+//     i=0;
+//     while(i<5){
+//         j=0;
+//         while(j<5){
+//             if(a[i][j]!=0){
+//             printf("%3d",a[i][j]);
+            
+//             }
+//             else{
+//                 printf("   ");
+//             }
+//             j++;
+//         }
+//         putchar('\n');
+        
+//         i++;
+//     }
+
+//     system("pause");
+// }
+
+
 int main(){
-    int i=0,j,a[5][5],n=1,k,m=0;
+    int a[5][5] = {0,};
+    int i=0,j,k=1,n,r=1,kk=0;
 
     while(i<5){
         j=0;
-        while(j<5){
-            if(j==0 && i >=1){
-                for(k=0;k<=m;k++){
+        if(i>0&&i<4){
+                for(n=1;n<=r;n++){
                     a[i][j]=0;
+                    //r++;
                     j++;
                 }
-                
-                m++;
+                if(i==2) r=1;
+                else r++;
             }
-            a[i][j]=n;
-            n++;
-            j++;
+        while(j<5){
+            if(i>0&&i<4){
+                if(i==3)kk=2;
+                if(j>4-i+kk){
+                    
+                    for(n=1;n<=i-kk;n++){
+                        a[i][j] = 0;
+                        j++;
+                    }
+                    
+                }
+                else{
+                    
+                    a[i][j] = k;
+                    k++;
+                    j++;
+            }
+            }
+            else{
+                a[i][j] = k;
+                k++;
+                j++;         
+           
+            }
         }
-        
-
         i++;
     }
-
     i=0;
     while(i<5){
         j=0;
         while(j<5){
             if(a[i][j]!=0){
-                printf("%3d",a[i][j]);
-                j++;
+            printf("%4d",a[i][j]);
+            
             }
-            else {
-                printf("   ");
-                j++;
+            else{
+                printf("    ");
             }
+            j++;
         }
-        i++;
         putchar('\n');
+        
+        i++;
     }
+
     system("pause");
+
+
 }
