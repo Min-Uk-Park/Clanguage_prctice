2f1d074650c69bd2907b9b1ad44e917e3a9938c6
ParkMinUk <parkmxxuk@gmail.com>
2023-06-30 17:27:15 +0900

    알고리즘(삽입정렬)

--- Cparctice.c
+++ Cparctice.c
@@ -5,11 +5,68 @@
 //#include <conio.h>
 #define MAX 100
 
-// 버블정렬
+// 삽입정렬
+// int main(){
+//     int a[MAX]={0,};
+
+//     int i,j,n,k,max,temp=0;
+
+//     for(i=0;i<MAX;i++){
+//        scanf("%d",&a[i]);
+//        if(a[i]==0)break;
+//     }
+
+//     n=i;
+    
+
+//     k=1;
+//     for(i=1;i<n;i++){
+//         for(i=0;i<k;i++){
+//             if(a[i]>a[k]){
+//                 temp = a[i];
+//                 a[i]=a[k];
+//                 a[k]=temp;
+//             }
+//         }
+//         k++;
+//     }
+    
+    
+//     for(i=0;i<n;i++){
+        
+//         printf("%3d",a[i]);
+//     }
+    
+//     putchar('\n');
+
+//     system("pause");
+// }
+
+// 삽입정렬
+void insert_sort(int a[],int n){
+    int i,j,temp,k;
+
+    for(i=1;i<n;i++){
+        k=i-1;
+        j=k;
+        while(1){
+            if(a[j]>a[i]){
+                temp = a[j];
+                a[j] = a[i];
+                a[i] = temp;
+                i--;
+            }
+            
+            if(j==0) break;
+            else j--;
+        }
+    }
+}
+
 int main(){
     int a[MAX]={0,};
 
-    int i,j,n,nn,max,temp=0;
+    int i,j,n,k,max,temp=0;
 
     for(i=0;i<MAX;i++){
        scanf("%d",&a[i]);
@@ -17,40 +74,17 @@ int main(){
     }
 
     n=i;
-    nn=i;
+
+    insert_sort(a,n);
 
     for(i=0;i<n;i++){
-        printf("%3d",a[i]);
-    }
     
-    putchar('\n');
+    printf("%3d",a[i]);
 
-    
-
-    for(i=0;i<nn-1;i++){
-        max =a[0]; 
-        for(j=1;j<n;j++){
-            
-            if(max>a[j]){
-                temp = a[j];
-                a[j] = max;
-                a[j-1] = temp;
-                max = a[j];
-            }
-            else {max = a[j];
-                a[j] = max;}
-            
-        }
-        n--;
-    }
-
-    
-    for(i=0;i<nn;i++){
-        
-        printf("%3d",a[i]);
     }
     
     putchar('\n');
 
     system("pause");
+
 }
