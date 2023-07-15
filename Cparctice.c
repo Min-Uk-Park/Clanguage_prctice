5b0504bfd2c91edb4d7e9320ec6b4ccb9eb1ffdb
ParkMinUk <parkmxxuk@gmail.com>
2023-07-15 22:38:55 +0900

    포인터(함수포인터)

--- Cparctice.c
+++ Cparctice.c
@@ -2,50 +2,46 @@
 #include <stdlib.h>
 #define MAX 100
 
-// 값 전달(call by value) -> 인수(그 변수의 값)가 전달
-// void exchange(int i,int j){
-//     int temp;
-
-//     temp = i;
-//     i=j;
-//     j=temp;
-
-// }
-
-// int main(){
-
-//     int i=10,j=20;
-
-//     printf("함수 호출 전 i,j값 : %d, %d\n",i,j);
-
-//     exchange(i,j);
-
-//     printf("함수 호출 후 i,j값 : %d, %d\n",i,j);
-
-//     system("pause");
-// }
-
-
-
-// 주소 전달(call by reference) -> 그 변수의 주소를 전달
-void exchange(int *i,int *j){
-    int temp;
-
-    temp = *i;
-    *i=*j;
-    *j=temp;
-
-}
+int add(int a,int b){return a+b;}
+int sub(int a,int b){return a-b;}
+int divide(int a,int b){return a/b;}
+int mul(int a,int b){return a*b;}
 
 int main(){
+    int (* operation)(int ,int ); //함수 포인터 생성
+    int a,b;
 
-    int i=10,j=20;
+    char  op;
+    printf("a와 b의 값을 입력하세요. ");
+    scanf("%d%d",&a,&b);
+    printf("연산자를 입력하세요. ");
+    getchar();
+    op = getchar();
+    
+// if문 사용한 경우    
+    // while(1){
+    // if(op=='+'){operation=add;break;}
+    // else if(op == '-'){operation=sub;break;}
+    // else if(op == '/'){operation=divide;break;}
+    // else if(op == '*'){operation=mul;break;}
+    // else {printf("error 다시 입력하세요. ");getchar();op = getchar();}
+    // }
 
-    printf("함수 호출 전 i,j값 : %d, %d\n",i,j);
+// switch~case 사용한 경우
+    int k=1;
+    while(k){
+        switch(op){
+            case '+' : operation = add; k=0; break; 
+            case '-' : operation = sub; k=0; break;
+            case '/' : operation = divide; k=0; break;
+            case '*' : operation = mul; k=0; break;
+            default : {printf("error, 다시 입력하세요. "); getchar();  op = getchar();}
+        }
+        
+    }
 
-    exchange(&i,&j);
 
-    printf("함수 호출 후 i,j값 : %d, %d\n",i,j);
+    printf("%d %c %d = %d\n",a,op,b,operation(a,b));
 
     system("pause");
 }
