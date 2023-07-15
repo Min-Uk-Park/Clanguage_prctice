d749abcc7e742fb0bea23ae1809ae9675adf3415
ParkMinUk <parkmxxuk@gmail.com>
2023-07-15 21:20:09 +0900

    포인터(주소전달, 값전달)

--- Cparctice.c
+++ Cparctice.c
@@ -2,54 +2,50 @@
 #include <stdlib.h>
 #define MAX 100
 
-// int main(){
-//     int a=50;
-//     char name[MAX] = "parkminuk";
-//     // char name[a] = "parkminuk"; //배열의 크기 변수로 입력받아 사용 못함
+// 값 전달(call by value) -> 인수(그 변수의 값)가 전달
+// void exchange(int i,int j){
+//     int temp;
 
-//     printf("%s\n",name);
+//     temp = i;
+//     i=j;
+//     j=temp;
+
+// }
+
+// int main(){
+
+//     int i=10,j=20;
+
+//     printf("함수 호출 전 i,j값 : %d, %d\n",i,j);
+
+//     exchange(i,j);
+
+//     printf("함수 호출 후 i,j값 : %d, %d\n",i,j);
+
 //     system("pause");
 // }
 
-// ??????????**
-// int main(){
-//     int k[5]={1,2,3,4,5},*kp;
 
-//     kp = k;
 
-//     // kp+=2;
+// 주소 전달(call by reference) -> 그 변수의 주소를 전달
+void exchange(int *i,int *j){
+    int temp;
 
-//     printf("%d\n",kp);
+    temp = *i;
+    *i=*j;
+    *j=temp;
 
-//     kp++; //  이 말의 의미는 &k[0] -> &k[1]의 의미? 왜 그런지 이해 안됨 주소값의 차이는 4byte씩 나는 거 아닌가
-
-//     printf("%d\n",kp);
-
-//     system("pause");
-// }
+}
 
 int main(){
-    int n,sum;
-    int *p;
 
-    printf("입력할 수의 갯수를 입력하시오. :");
-    scanf("%d",&n);
+    int i=10,j=20;
 
-    p = (int*)malloc(sizeof(int)*n); // p는 크기가 4byte인 n개의 배열 중 1번째 배열의 주소를 의미함
+    printf("함수 호출 전 i,j값 : %d, %d\n",i,j);
 
-    int i=0;
-    while(i<n){
-        scanf("%d",(p+i)); // ?????????**
-        // p++;
-        i++;
-    }
+    exchange(&i,&j);
 
-    
-    for(i=0;i<n;i++){
-        printf("p[%d] => %d\n",i,*(p+i));
-        
-    }
+    printf("함수 호출 후 i,j값 : %d, %d\n",i,j);
 
     system("pause");
-
 }
