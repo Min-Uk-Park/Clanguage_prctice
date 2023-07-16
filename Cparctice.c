1b00bf2576c01b6a90ae1bf73c857d517b3c5094
ParkMinUk <parkmxxuk@gmail.com>
2023-07-16 18:12:13 +0900

    구조체(구조체 초기화)

--- Cparctice.c
+++ Cparctice.c
@@ -1,41 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <stddef.h> // offsetof함수 사용 시 선언 
+#include <string.h> // memset함수 사용 시 선언
 
-// offsetof는 상대주소이다. 변위차로 첫 번째 주소로부터 얼마나 떨어져있는지를 나타냄
-struct score{
-    char name[20]; // 20
-    float math,kor,eng; //12
+struct student{
+    int age;
+    char name[10];
+    float grade;
 };
 
-struct group{
-    
-    struct score member[3];
+// int main(){
+//     struct student aaa = {0, };
 
-};
+//     printf("%d\n",aaa.age);
+//     printf("%s\n",aaa.name);
+//     printf("%.1f\n",aaa.grade);
+
+//     system("pause");
+// }
 
 int main(){
+    struct student *aaa;
 
-    struct group group1;
+    aaa = malloc(sizeof(struct student));
 
-    int a,b,c,d,e,f,g,h,i;
+    memset(aaa,0,sizeof(struct student)); // 초기화는 0으로 해야함!!!
 
-    a = sizeof(group1.member[0]); //32
-    b = sizeof(group1.member[1]); //32
-    c = sizeof(group1.member[2]); //32
-
-    d = sizeof(group1); // 96
-
-    e = offsetof(struct score,name);
-    f = offsetof(struct score,math);
-    g = offsetof(struct score,eng);
-
-    h = offsetof(struct group,member[0]);
-    i = offsetof(struct group,member[1]);
-
-    printf("%3d%3d%3d%3d\n",a,b,c,d);
-    printf("%3d%3d%3d\n",e,f,g);
-    printf("%3d%3d\n",h,i);
+    printf("%d\n",(*aaa).age);
+    printf("%s\n",(*aaa).name);
+    printf("%.1f\n",(*aaa).grade);
 
     system("pause");
 }
