6ce9bc7d47a1c6df7c1e10666e441f46a7554e7e
ParkMinUk <parkmxxuk@gmail.com>
2023-07-16 16:56:49 +0900

    구조체(구조체 배열)

--- Cparctice.c
+++ Cparctice.c
@@ -2,27 +2,32 @@
 #include <stdlib.h>
 
 struct student{
-    char *name;
+    char name[50];  //-> 포인터는 왜 실행이 안되는가
     int age;
     float grade;
-}*member;
+}member[2];
 
 
 int main(){
-    member = malloc(sizeof(struct student));
+    int i=0;
 
-    (*member).name = "parkminuk";
-    member->age = 23;
-    member->grade = 70.7;
+    while(i<2){
+        printf("이름 나이 성적을 차례대로 입력하시오. : ");
+        scanf("%s%d%f",member[i].name,&member[i].age,&member[i].grade);
+        getchar();
+        i++;
+    }
+    
+    printf("%9s%9s%9s\n","이름","나이","성적");
+    puts("================================\n");
 
-    printf("%s\n%d\n%.1f\n",(*member).name,member->age, member->grade);
-    printf("%d\n",sizeof(struct student));
-
-    (*(member+10)).name = "kimminsuk";
-    (member+10)->age = 20;
-    (member+10)->grade = 66.6;
-    printf("%s\n%d\n%.1f\n",(*(member+10)).name,(member+10)->age, (member+10)->grade);
-
+    i=0;
+    while(i<2){
+        printf("%6s%6d%9.1f\n",member[i].name,member[i].age,member[i].grade);
+        i++;
+    }
     system("pause");
 }
 
+
+ 
