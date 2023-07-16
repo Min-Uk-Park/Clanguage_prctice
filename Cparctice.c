3c6244b3f091ef1ccf02d3bdcf998168c7cfe810
ParkMinUk <parkmxxuk@gmail.com>
2023-07-16 18:19:45 +0900

    구조체(구조체 대입)

--- Cparctice.c
+++ Cparctice.c
@@ -8,26 +8,16 @@ struct student{
     float grade;
 };
 
-// int main(){
-//     struct student aaa = {0, };
-
-//     printf("%d\n",aaa.age);
-//     printf("%s\n",aaa.name);
-//     printf("%.1f\n",aaa.grade);
-
-//     system("pause");
-// }
-
 int main(){
-    struct student *aaa;
+    struct student stu1 = {21,"parkminuk",77.7};
+    struct student stu2;
+    struct student *stu3;
 
-    aaa = malloc(sizeof(struct student));
+    stu2=stu1; // 구조체 변수끼리 대입이 가능하다.
+    stu3=&stu1; // 구조체 포인터 변수에 구조체 변수의 주소 대입이 가능하다.
 
-    memset(aaa,0,sizeof(struct student)); // 초기화는 0으로 해야함!!!
-
-    printf("%d\n",(*aaa).age);
-    printf("%s\n",(*aaa).name);
-    printf("%.1f\n",(*aaa).grade);
+    printf("%d  %s  %.1f\n",stu2.age,stu2.name,stu2.grade);
+    printf("%d  %s  %.1f\n",stu3->age,stu3->name,stu3->grade);
 
     system("pause");
 }
